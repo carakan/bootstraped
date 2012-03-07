11ad2dba217f540d654a108c26b4e59964a866f6
Carlos Ramos <carakan@gmail.com>
2012-03-06 23:06:10 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nuki::Application.config.secret_token = 'a47de6c44e5a21c0186af3d3fc313ace941c4eb8172526ce02dbf2ac8d3953561960a3c987f3f90498b7c831cc5d0fc28857c78df434c18f8e1c510f9c3cc4c7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Nuki::Application.config.session_store :cookie_store, :key => '_nuki_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Nuki::Application.config.session_store :active_record_store
